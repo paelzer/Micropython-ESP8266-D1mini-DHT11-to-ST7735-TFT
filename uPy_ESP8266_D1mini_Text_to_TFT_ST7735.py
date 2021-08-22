c64ec5da7c64db6802b9d395eac4475bf0d176e8
p43lz3r <16374477+paelzer@users.noreply.github.com>
2021-08-22 23:17:45 +0200

    Update uPy_ESP8266_D1mini_Text_to_TFT_ST7735.py

--- uPy_ESP8266_D1mini_Text_to_TFT_ST7735.py
+++ uPy_ESP8266_D1mini_Text_to_TFT_ST7735.py
@@ -3,36 +3,40 @@ from time import sleep
 import st7735
 import bitmapfont
 
+# DHT11 sensor related library
+from dht import DHT11
+
 display = st7735.ST7735R(SPI(1, baudrate=40000000), dc=Pin(16), cs=Pin(15), rst=Pin(0))
 
 bf = bitmapfont.BitmapFont(160,128, display.pixel)
 bf.init()
 
 # sensor values initially 0
-x = 0
-y = 0
-z = 0
+dht11sensor = DHT11(Pin(5))
+
+#temp = dht11sensor.temperature()
+#hum = dht11sensor.humidity()
+
+temp = 0
+hum = 0
 
 # store old sensor values
-oldX = x
-oldY = y
-oldZ = z
+oldTemp = temp
+oldHum = hum
 
 # name column x - position
 nameCol = 7
 
 # y - positions
-xRow = 30
-yRow = 45
-zRow = 60
+tempRow = 30
+humRow = 45
 
 # x - position for sensor values
 valCol = 80
 
 # names for the sensor
-xName = "Sensor 1:"
-yName = "Sensor 2:"
-zName = "Sensor 3:"
+nameTemp = "Temperatur:"
+nameHum = "Humidity:"
 
 # clear the display
 display.fill(0)
@@ -41,9 +45,8 @@ display.fill(0)
 bf.text("Some sensor values...", 0, 0, 0xffff)
 
 # put sensor values names on the display
-bf.text(xName, nameCol, xRow, 0xffff)
-bf.text(yName, nameCol, yRow, 0xffff)
-bf.text(zName, nameCol, zRow, 0xffff)
+bf.text(nameTemp, nameCol, tempRow, 0xffff)
+bf.text(nameHum, nameCol, humRow, 0xffff)
 
 # draw a table
 # display.h/vline(x, y, length, color)
@@ -55,26 +58,21 @@ display.vline(68,17, 66, color=0xffff)
 
 # endless loop
 while(True):
-    if not x == oldX: # only overwrite and delete row if new sensor value is different from old one
-        bf.text(str(oldX), valCol, xRow, 0x00) # delete row
-        bf.text(str(x), valCol, xRow, 0xfff) # put new value on the display
+    if not temp == oldTemp: # only overwrite and delete row if new sensor value is different from old one
+        bf.text(str(oldTemp), valCol, tempRow, 0x00) # delete row
+        bf.text(str(temp), valCol, tempRow, 0xfff) # put new value on the display
 
-    if not y == oldY:
-        bf.text(str(oldY), valCol, yRow, 0x00)
-        bf.text(str(y), valCol, yRow, 0xfff)
+    if not hum == oldHum:
+        bf.text(str(oldHum), valCol, humRow, 0x00)
+        bf.text(str(hum), valCol, humRow, 0xfff)
 
-    if not z == oldZ:
-        bf.text(str(oldZ), valCol, zRow, 0x00)
-        bf.text(str(z), valCol, zRow, 0xfff)
-    
     # store old sensor values for overwrite purpose
-    oldX = x
-    oldY = y
-    oldZ = z
+    oldTemp = temp
+    oldHum = hum
     
-    # fake sensor values changes (could be replaced by real sensor readings)
-    x += 1
-    y += 5
-    z += 7
+    # get new sensor values
+    dht11sensor.measure()
+    temp = dht11sensor.temperature()
+    hum = dht11sensor.humidity()
     
-    sleep(1)
+    sleep(5)
